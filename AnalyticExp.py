210d0acc3520e3f8da60af270b9fb3b38a3c5245
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-02-17 16:51:26 -0500

    in the progress of solving the transcendal equation, as it does nto work for some cases

--- AnalyticExp.py
+++ AnalyticExp.py
@@ -1,11 +1,13 @@
 import numpy as np
+from scipy.optimize import bisect
 
 def calculate_cycloid_radius(lx, ly):
-    pass
+    f = lambda r: r*(np.cos(1-ly/r)-np.sqrt(1-(1-ly/r)**2))-lx
+    return bisect(f, 0.5*ly, ly)
 
 def generate_cycloid(lx, ly, numpts=101):
     r = calculate_cycloid_radius(lx, ly)
-    t = np.linspace(0, np.arccos(1.-ly/r), numpts)
+    t = np.linspace(0, 2*np.pi-np.arccos(1.-ly/r), numpts)
     x = r * (t-np.sin(t))
     y = ly - r * (1-np.cos(t))
     return x, y
