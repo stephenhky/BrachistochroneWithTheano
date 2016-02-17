55d83b78a3a9180ad75ce21d3873b04809063c96
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-02-17 18:22:11 -0500

    updated the transcendental equation. now it works when lx>ly

--- AnalyticExp.py
+++ AnalyticExp.py
@@ -1,13 +1,13 @@
 import numpy as np
-from scipy.optimize import bisect
+from scipy.optimize import fsolve
 
 def calculate_cycloid_radius(lx, ly):
-    f = lambda r: r*(np.cos(1-ly/r)-np.sqrt(1-(1-ly/r)**2))-lx
-    return bisect(f, 0.5*ly, ly)
+    f = lambda xi: np.arccos(1-xi) - np.sqrt(1-(1-xi)**2) - lx/ly*xi
+    return ly/fsolve(f, 1.2)
 
 def generate_cycloid(lx, ly, numpts=101):
     r = calculate_cycloid_radius(lx, ly)
-    t = np.linspace(0, 2*np.pi-np.arccos(1.-ly/r), numpts)
+    t = np.linspace(0, np.arccos(1.-ly/r), numpts)
     x = r * (t-np.sin(t))
     y = ly - r * (1-np.cos(t))
     return x, y
