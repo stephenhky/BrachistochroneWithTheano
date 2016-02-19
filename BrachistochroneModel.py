b46414a1536458778f98ff1f87e357c522b16a42
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-02-19 10:12:01 -0500

    loop working now by using single-handed derivative

--- BrachistochroneModel.py
+++ BrachistochroneModel.py
@@ -2,26 +2,27 @@ import theano
 import theano.tensor as T
 
 def brachitochrone_functional():
+    # define all symbols
     lx, ly = T.dscalars('lx', 'ly')
     fseq = T.dvector('fseq')
     N = fseq.size
     delta_x = lx / N
     iseq = T.arange(N)
 
-    dfseq, _ = theano.map(fn=lambda i: T.switch(T.eq(i, 0),
-                                                fseq[1]-fseq[0],
-                                                T.switch(T.eq(i, N-1),
-                                                         fseq[N-1]-fseq[N-2],
-                                                         0.5*(fseq[i+1]-fseq[i-1]))) / delta_x,
+    # derivatives array
+    dfseq, _ = theano.map(fn=lambda i: T.switch(T.eq(i, 0), fseq[1]-fseq[0], fseq[i]-fseq[i-1]) / delta_x,
                           sequences=[iseq])
 
+    # functional term
     functional_ithterm = lambda i: delta_x * T.sqrt(0.5*(1+(dfseq[i])**2)/(ly-fseq[i]))
 
+    # defining the functions
     functional_parts, _ = theano.map(fn=lambda k: functional_ithterm(k), sequences=[iseq])
     functional = functional_parts.sum()
     gfunc = T.grad(functional, fseq)
 
-    time = theano.function(inputs=[fseq, lx, ly], outputs=functional)
-    grad_time = theano.function(inputs=[fseq, lx, ly], outputs=gfunc)
+    # compile the functions
+    time_fcn = theano.function(inputs=[fseq, lx, ly], outputs=functional)
+    grad_time_fcn = theano.function(inputs=[fseq, lx, ly], outputs=gfunc)
 
-    return time, grad_time
+    return time_fcn, grad_time_fcn
