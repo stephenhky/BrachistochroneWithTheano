c4d74641374ef973420d1eab2802af5f9f4b4cb2
stephenhky <cwin31>
2016-02-18 22:00:39 -0500

    simplying code and logic... still not working

--- BrachistochroneModel.py
+++ BrachistochroneModel.py
@@ -3,26 +3,25 @@ import theano.tensor as T
 
 def brachitochrone_functional():
     lx, ly = T.dscalars('lx', 'ly')
-    N = T.iscalar('N')
+    fseq = T.dvector('fseq')
+    N = fseq.size
     delta_x = lx / N
     iseq = T.arange(N)
-    fseq = T.dvector('fseq')
 
-    functional_1stterm = delta_x * T.sqrt(0.5*(1+((fseq[0]-ly)/delta_x)**2)/(ly-fseq[0]))
-    functional_lastterm = delta_x * T.sqrt(0.5*(1+((fseq[N-1]-fseq[N-2])/delta_x)**2)/(ly-fseq[N-1]))
-    functional_ithterm = lambda i: delta_x * T.sqrt(0.5*(1+((fseq[i+1]-fseq[i-1])/(2*delta_x))**2)/(ly-fseq[i]))
+    dfseq, _ = theano.map(fn=lambda i: T.switch(T.eq(i, 0),
+                                                fseq[1]-fseq[0],
+                                                T.switch(T.eq(i, N-1),
+                                                         fseq[N-1]-fseq[N-2],
+                                                         0.5*(fseq[i+1]-fseq[i-1]))) / delta_x,
+                          sequences=[iseq])
 
-    pfunc = lambda i: T.switch(T.eq(i, 0),
-                               functional_1stterm,
-                               T.switch(T.eq(i, N-1),
-                                        functional_lastterm,
-                                        functional_ithterm(i)))
+    functional_ithterm = lambda i: delta_x * T.sqrt(0.5*(1+(dfseq[i])**2)/(ly-fseq[i]))
 
-    functional_parts = theano.map(fn=lambda k: pfunc(k), sequences=[iseq])
+    functional_parts, _ = theano.map(fn=lambda k: functional_ithterm(k), sequences=[iseq])
     functional = functional_parts.sum()
     gfunc = T.grad(functional, fseq)
 
-    time = theano.function(inputs=[fseq, lx, ly, N], outputs=functional)
-    grad_time = theano.function(inputs=[fseq, lx, ly, N], outputs=gfunc)
+    time = theano.function(inputs=[fseq, lx, ly], outputs=functional)
+    grad_time = theano.function(inputs=[fseq, lx, ly], outputs=gfunc)
 
     return time, grad_time
